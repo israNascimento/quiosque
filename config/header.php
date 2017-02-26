9ded9229b4639215fe9f83b5714c0f0f7927a003
Israel Nascimento <dev.israelnascimento@gmail.com>
2017-02-26 09:28:12 -0300

    Commit inicial

---
+++ config/header.php
@@ -0,0 +1,8 @@
+<html>
+    <header>
+        <title>Quiosque</title>
+        <link rel="icon" href="./image/favicon.png" /> <!--A pagina que esta chamando é a index.html -->
+        <meta charset="utf-8" />
+    </header>
+    <body>
+        <main>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+    require_once("config/header.php");
+?>
+<h1>Bem vindo ao quiosque.</h1>
+<?php
+    require_once("config/footer.php");
+?>
