a486d4b8869eebd89d08dbfd1c363bb240817324
Israel Nascimento <dev.israelnascimento@gmail.com>
2017-02-26 09:38:04 -0300

    Adicionado bootstrap

--- config/header.php
+++ config/header.php
@@ -2,7 +2,9 @@
     <header>
         <title>Quiosque</title>
         <link rel="icon" href="./image/favicon.png" /> <!--A pagina que esta chamando é a index.html -->
+        <link rel="stylesheet" type="text/css" href="./style/style.css"  />
+        <link rel="stylesheet" type="text/css" href="./style/bootstrap.css"  />
         <meta charset="utf-8" />
     </header>
     <body>
-        <main>
+        <main class="container">
