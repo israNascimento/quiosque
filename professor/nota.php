add5f353949a73c2feb467a5c1d8460f75d11e5c
Israel Nascimento <dev.israelnascimento@gmail.com>
2017-03-01 16:56:34 -0300

    Corrigido bug ao entrar pela primeira vez na pagina de notas

--- professor/nota.php
+++ professor/nota.php
@@ -10,51 +10,48 @@
     $turmas = $professorDAO->getTurmas(getUserId());
     $selected = 0;
 ?>
-<select id="select" class="form-control" onchange="changeClass()">
-    <?php
-        foreach ($turmas as $turma) {
-            $select = "";
-            if(isset($_GET['codigo']) && $turma->getId() == $_GET['codigo']) {
-                $select = "selected=selected";
-                $selected = $turma;
-            }?>
-            <option value="<?=$turma->getId()?>" <?=$select?>><?=$turma->getId()?> - <?=$turma->getCodigo()?> - <?=$turma->getNome()?></option>
-    <?php }
-    ?>
-</select>
-
+<form id="form-turmas" action="nota.php" method="post" >
+    <select id="select" name="select" class="form-control" onchange="changeClass()">
+        <?php
+            foreach ($turmas as $turma) {
+                $select = "";
+                if(isset($_POST['select']) && $turma->getId() == $_POST['select']) {
+                    $select = "selected=selected";
+                    $selected = $turma;
+                }?>
+                <option value="<?=$turma->getId()?>" <?=$select?>><?=$turma->getId()?> - <?=$turma->getCodigo()?> - <?=$turma->getNome()?></option>
+        <?php }
+            if(!isset($_POST['select'])) {
+                $selected = reset($turmas);
+            }
+        ?>
+    </select>
+</form>
 <script>
     var select = document.getElementById("select");
     var changeClass = function() {
-        window.location = "nota.php?codigo="+select.options[select.selectedIndex].value;
+        var form = document.getElementById("form-turmas");
+        form.submit();
     }
 </script>
-
-<?php
-    if($selected === 0 && $_GET['codigo']) {
-        echo "Turma não encontrada para o professor. Você é malicioso?";
-        die();
-    }
-    ?>
-    <table class="table">
+<table class="table">
+    <tr>
+        <th>Aluno</th>
+        <th>Nota</th>
+    </tr>
+    <?php
+    foreach ($selected->getAlunos() as $aluno) { ?>
         <tr>
-            <th>Aluno</th>
-            <th>Nota</th>
+            <td>
+                <?=$aluno->getNome()?>
+            </td>
+            <td>
+                <?=10?>
+            </td>
         </tr>
-        <?php
-        foreach ($selected->getAlunos() as $aluno) { ?>
-            <tr>
-                <td>
-                    <?=$aluno->getNome()?>
-                </td>
-                <td>
-                    <?=10?>
-                </td>
-            </tr>
-        <?php }
-        ?>
-    </table>
-
+    <?php }
+    ?>
+</table>
 <?php
     require_once("../config/footer.php");
 ?>
